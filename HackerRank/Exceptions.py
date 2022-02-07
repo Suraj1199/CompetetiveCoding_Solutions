ca7ebf3ada5fc4d69a22a9070c26fb4dea6920b3
Suraj1199 <suraj19995.ss@gmail.com>
2022-02-07 23:55:36 +0530

    Create Exceptions.py

---
+++ HackerRank/Exceptions.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    try:
+        a, b = map(int, input().split(" "))
+        print(a // b)
+    except ValueError as e:
+        print('Error Code:', e)
+    except ZeroDivisionError:
+        print('Error Code: integer division or modulo by zero')
+               
+        
