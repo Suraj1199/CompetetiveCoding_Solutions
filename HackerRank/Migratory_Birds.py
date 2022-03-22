45c22d1d4c70d5583ca613cea8fb85e3c9cc979b
Suraj1199 <suraj19995.ss@gmail.com>
2022-03-22 23:14:23 +0530

    Update Migratory_Birds.py

--- HackerRank/Migratory_Birds.py
+++ HackerRank/Migratory_Birds.py
@@ -1,13 +1,4 @@
 from collections import Counter
-
-def migratoryBirds(arr):
-    h = Counter(arr)
-    print(h)
-    return sorted(h, key=h.get, reverse=True)[0]
-
-if __name__ == '__main__':
-    arr_count = int(input())
-    arr = list(map(int, input().rstrip().split(" ")))
-    result = migratoryBirds(arr)
-
-    print(result)
+n = int(input())
+c = Counter(input().split())
+print(max(c, key=lambda x: (c[x], -int(x))))
