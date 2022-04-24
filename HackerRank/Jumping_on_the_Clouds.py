16c786e492e17c58be051061ca2f6b9fac0125fc
Suraj1199 <suraj19995.ss@gmail.com>
2022-04-24 23:34:02 +0530

    Create Jumping_on_the_Clouds.py

---
+++ HackerRank/Jumping_on_the_Clouds.py
@@ -0,0 +1,13 @@
+def jumpingOnClouds(c):
+    if len(c) == 1 : return 0
+    if len(c) == 2: return 0 if c[1]==1 else 1
+    if c[2]==1: 
+        return 1 + jumpingOnClouds(c[1:])
+    if c[2]==0:
+        return 1 + jumpingOnClouds(c[2:])
+    
+if __name__ == '__main__':
+    n = int(input().strip())
+    c = list(map(int, input().rstrip().split()))
+    result = jumpingOnClouds(c)
+    print(result)
