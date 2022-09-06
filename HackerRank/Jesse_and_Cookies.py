ee6d2ccf88a065f7186d642a4bd267dbaa8bb71b
Suraj1199 <suraj19995.ss@gmail.com>
2022-09-06 23:18:04 +0530

    Create Jesse_and_Cookies.py

---
+++ HackerRank/Jesse_and_Cookies.py
@@ -0,0 +1,36 @@
+def cookies(k, A):
+    # Write your code here
+    A = sorted(A, reverse = True)
+    count = 0
+    temp = []
+    l = 0
+    while A and A[-1] < k or temp and temp[l] < k:
+        if len(A) + len(temp[l:l+2]) < 2:
+            count = -1
+            break            
+        else:
+            count += 1
+            a0 = []
+            for num, i in enumerate(range(min(2, len(A)))): 
+                a0.append((A[-num-1], 0))
+            a1 = []
+            tmp = temp[l:l+2]
+            for num, i in enumerate(range(min(2, len(tmp)))): 
+                a1.append((tmp[num], 1))
+            b = sorted(a0+a1, key=lambda x: x[0])[:2]
+            for i in range(2):
+                if b[i][1] == 0:
+                    A.pop()
+                else:
+                    l += 1
+            temp.append(b[0][0] + b[1][0]*2)            
+    return count
+
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+    n = int(first_multiple_input[0])
+    k = int(first_multiple_input[1])
+    A = list(map(int, input().rstrip().split()))
+    result = cookies(k, A)
+    print(result)
